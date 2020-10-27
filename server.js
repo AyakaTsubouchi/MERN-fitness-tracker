20e9edf5d1d670652fff0627de07e92265216ab3
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-10-27 15:58:11 -0700

    test

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('/servertest', (req, res) => {
 //     });
 
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
