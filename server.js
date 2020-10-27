1e3ca5da2379e6ea70156c9cb7c11c4a3123fc61
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-10-27 16:04:58 -0700

    test

--- server.js
+++ server.js
@@ -42,17 +42,17 @@ app.get('/servertest', (req, res) => {
 //   })
 // }
 
-// mongoose.set('useCreateIndex', true)
-// mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true  })
-//     .then(() => {
-//         console.log('Connected!');
-//         app.listen(port);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
+mongoose.set('useCreateIndex', true)
+mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true  })
+    .then(() => {
+        console.log('Connected!');
+        app.listen(port);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
