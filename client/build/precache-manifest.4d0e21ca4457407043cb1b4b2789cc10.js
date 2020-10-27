d36cc1122d04b9f915be15672190912b3ac0ed20
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-10-26 22:21:26 -0700

    ready to deploy

---
+++ client/build/precache-manifest.4d0e21ca4457407043cb1b4b2789cc10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c713f4e240e5adeaa01f3ecef733c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b68b8ce69ac511057b",
+    "url": "/static/css/2.293e860e.chunk.css"
+  },
+  {
+    "revision": "190f14e21fc25a4ef651",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82b68b8ce69ac511057b",
+    "url": "/static/js/2.470ba457.chunk.js"
+  },
+  {
+    "revision": "481e605ffa29129da3caf5a13f3eb183",
+    "url": "/static/js/2.470ba457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190f14e21fc25a4ef651",
+    "url": "/static/js/main.291ab83b.chunk.js"
+  },
+  {
+    "revision": "3b4bc318b0ec17c94cf2",
+    "url": "/static/js/runtime-main.46040147.js"
+  }
+]);
