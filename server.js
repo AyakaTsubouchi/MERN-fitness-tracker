46046b73c91ed0c2cca423f1adcb22b9a6cfffd7
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-10-27 12:21:18 -0700

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require('path')
@@ -8,8 +9,9 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json({ extended: false }));
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
